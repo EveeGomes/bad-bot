da9ab4acc42265a8d35ac8b6c5cda43910786030
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-10-13 10:25:23 -0300

    Add initial files.

---
+++ Source/BadBot/Actor/Bot.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/Bot.h"
+
+// Sets default values
+ABot::ABot()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
